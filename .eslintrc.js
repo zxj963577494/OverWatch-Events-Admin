e5f570f0ead919638859cfe848fbba939742f151
Zheng XuJiang <963577494@qq.com>
2018-10-05 00:03:42 +0800

    补充eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'no-underscore-dangle': 0,
+    'no-restricted-syntax': 0,
   },
   settings: {
     polyfills: ['fetch', 'promises', 'url'],
